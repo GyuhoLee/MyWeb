fe6c035dd2c2ddc94ee80b66c25f24d26d43e884
GyuhoLee <gyuho965@naver.com>
2019-06-07 04:25:30 +0900

    animation effect

--- js/agency.js
+++ js/agency.js
@@ -32,7 +32,7 @@
     offset: 56
   });
 
-  var height = window.innerHeight / 5 * 4;
+  var height = window.innerHeight * 0.6;
 
   // Collapse Navbar
   var navbarCollapse = function() {
@@ -42,9 +42,37 @@
       $("#mainNav").removeClass("navbar-shrink");
     }
     if($("#information").offset().top - window.scrollY < height){
+      $("#information").attr('style', 'visibility:visible');
       $("#infohead").addClass("zoomInDown");
       $("#infoleft").addClass("fadeInLeftBig");
       $("#inforight").addClass("fadeInRightBig");
+    }
+    if($("#portfolio").offset().top - window.scrollY < height){
+      $("#portfolio").attr('style', 'visibility:visible');
+      $("#porthead").addClass("bounceInUp");
+      $(".portfolio-item").addClass("fadeInUpBig");
+    }
+    if($("#about").offset().top - window.scrollY < height){
+      $("#about").attr('style', 'visibility:visible');
+      $("#abouthead").addClass("zoomIn");
+      $("#about1").attr('style', 'visibility:visible');
+      $("#about1").addClass("flipInX");
+    }
+    if($("#about2").offset().top - window.scrollY < height + 30){
+      $("#about2").attr('style', 'visibility:visible');
+      $("#about2").addClass("flipInX");
+    }
+    if($("#about3").offset().top - window.scrollY < height + 30){
+      $("#about3").attr('style', 'visibility:visible');
+      $("#about3").addClass("flipInX");
+    }
+    if($("#about-last").offset().top - window.scrollY < height + 30){
+      $("#about-last").attr('style', 'visibility:visible');
+      $("#about-last").addClass("flipInX");
+    }
+    if($("#contact").offset().top - window.scrollY < height){
+      $("#contact").attr('style', 'visibility:visible');
+      $("#contact").addClass("rollIn");
     }
   };
   // Collapse now if page is not at top
